a5621abb328e5ee524f43fe104728149029f349a
Ian Zapolsky <ianzapolsky@gmail.com>
2014-04-12 03:54:27 -0400

    pasted a lot of code from the original twitter-recommend bot to build the first functioning bot wired up to the oyster API

--- bot.py
+++ bot.py
@@ -1,29 +1,104 @@
-import os
-import oyster
+# bot.py, an experimentation with Twitter bots
+#
+# Replies to tweets with a book recommendation fetched via the Oyster API
+# based on hashtags contained within the tweet.
+#
+# by Ian Zapolsky - 3.29.14
 
+import os
+import time
+
+import oyster
+from twitter import Twitter, OAuth, TwitterHTTPError
+
+# name of the username the bot will operate under
+BOT_NAME = 'ianzapolsky'
+
+# Oyster API credentials
 OYSTER_ID     = os.environ['OYSTER_ID']
 OYSTER_SECRET = os.environ['OYSTER_SECRET']
 ACCESS_KEY    = os.environ['ACCESS_KEY']
 
-auth = oyster.auth.OysterOAuthHandler(
-    client_id     = OYSTER_ID, 
+# Twitter API credentials
+OAUTH_TOKEN     = os.environ['OAUTH_TOKEN']
+OAUTH_SECRET    = os.environ['OAUTH_SECRET']
+CONSUMER_KEY    = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+
+# return all tweets mentioning @BOT_NAME that have been created since latest_id
+def fetch_unseen_mentions(latest_id):
+  return t.search.tweets(q='@'+BOT_NAME, result_type='recent', since_id=latest_id)['statuses']
+
+# return the id of the latest tweet mentioning @BOT_NAME
+def fetch_latest_id():
+  return t.search.tweets(q='@'+BOT_NAME, result_type='recent', count=1)['statuses'][0]['id']
+
+# convert hashtags dict into a space-separated string
+def tags_to_string(hashtags):
+  tag_list = ''
+  for tag in hashtags:
+    tag_list += tag['text']+' '
+  return tag_list
+
+if __name__ == '__main__':
+
+  # initialize Twitter connection
+  t = Twitter(auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET,
+                         CONSUMER_KEY, CONSUMER_SECRET))
+
+  # initialize Oyster connection
+  auth = oyster.auth.OysterOAuthHandler(
+    client_id     = OYSTER_ID,
     client_secret = OYSTER_SECRET,
     oauth_host    = 'api.oysterbooks.com'
-)
-auth.access_token = ACCESS_KEY
-
-api  = oyster.API(
-    auth_handler = auth, 
+  )
+  auth.access_token = ACCESS_KEY
+  o = oyster.API(
+    auth_handler = auth,
     host         = 'api.oysterbooks.com',
-)
+  )
 
-'''
-auth = oyster.auth.OysterOAuthHandler(OYSTER_ID, OYSTER_SECRET)
-auth.access_token = ACCESS_KEY
-api = oyster.API(auth, host='api.oysterbooks.com')
-'''
+  # initialize latest_id to the most recent mention of @BOT_NAME
+  latest_id = fetch_latest_id()
 
-x = api.search('Steinbeck')
+  # infinite loop to continously reply to new, properly formatted tweets
+  while True:
 
-print x[0].title
+    print 'waking up!'
+    results = fetch_unseen_mentions(latest_id)
+    count = 0
+
+    if not results:
+      print 'no new tweets.'
+    else:
+      for tweet in reversed(results):
+
+        tweeter  = tweet['user']['screen_name']
+        hashtags = tweet['entities']['hashtags']
+        tag_list = tags_to_string(hashtags)
+
+        if tag_list != '':
+          # search Oyster API
+          try:
+            oyster_books = o.search(tag_list)
+            for book in oyster_books:
+              msg = 'Hey @'+tweeter+'! Try "%s" by %s!' % (book.title,
+                                                           book.author)
+              if len(msg) <= 140:
+                t.statuses.update(status=msg)
+                count += 1
+                break
+          except:
+            t.statuses.update(
+              status="Hey @"+tweeter+"! We coudn't find any matches for those hashtags. Sorry!")
+            count += 1
+
+        latest_id = tweet['id']
+
+    # sleep 60 seconds at the end of each loop to avoid going over API
+    # restrictions (180 per 15-minute-window in 1.1)
+    print 'replied to '+str(count)+' tweets!'
+    print 'going to sleep...'
+    time.sleep(30)
+
 
